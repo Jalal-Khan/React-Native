a2abe1a8d57e485d5f76bf54d8ffee27a150a6cb
inzamam13 <inzamamofficials@gmail.com>
2023-12-20 22:07:49 -0800

    testing changes

--- App.tsx
+++ App.tsx
@@ -76,7 +76,7 @@ function App(): React.JSX.Element {
           style={{
             backgroundColor: isDarkMode ? Colors.black : Colors.white,
           }}>
-          <Section title="Inzamam Ul Haq">
+          <Section title="Inzamam Ul Haq i am woking">
             Edit <Text style={styles.highlight}>App.tsx</Text> to change this
             screen and then come back to see your edits.
           </Section>
